cfa9c08d721cfc3abcbd4d78bfcc2aa135e447cf
mdtmc <miles.cranmer@gmail.com>
2015-10-09 00:47:26 -0400

    Trying to get actual url of file, with failure

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,4 +1,8 @@
 var url = window.location.href;
 var arxiv = url.substr(url.lastIndexOf('/') + 1);
 var voxsearch = "<br><a href=http://voxcharta.org/?s=" + arxiv+ "> Voxcharta </a>";
+var voxsearchh = "http://voxcharta.org/?s=" + arxiv;
+
+//document.write($('.product h3').load(voxsearchh).text());
+
 document.getElementsByClassName("title mathjax")[0].innerHTML += voxsearch;
