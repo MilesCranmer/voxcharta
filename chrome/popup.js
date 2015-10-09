342c6bd3ae17e2794cca626a031c0c70123d251b
mdtmc <miles.cranmer@gmail.com>
2015-10-09 00:26:09 -0400

    Working plugin that add voxcharta search link to arxiv abstract pages

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,4 +1,4 @@
 var url = window.location.href;
 var arxiv = url.substr(url.lastIndexOf('/') + 1);
-var voxsearch = "voxcharta.org/?s=" + arxiv;
-alert(voxsearch);
+var voxsearch = "<br><a href=http://voxcharta.org/?s=" + arxiv+ "> Voxcharta </a>";
+document.getElementsByClassName("title mathjax")[0].innerHTML += voxsearch;
