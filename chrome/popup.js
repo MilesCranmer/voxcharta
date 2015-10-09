73f362c891e717c4d5689da7ae489055dc5641f9
mdtmc <miles.cranmer@gmail.com>
2015-10-09 01:08:28 -0400

    sometimes arxiv adds other stuff to url. removed hopefully all of it

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,5 +1,6 @@
 var url = window.location.href;
 var arxiv = url.substr(url.lastIndexOf('/') + 1);
+arxiv = arxiv.substr(0,arxiv.lastIndexOf('?'));
 var voxsearch = "<br><a href=http://voxcharta.org/?s=" + arxiv+ "> Voxcharta </a>";
 var voxsearchh = "http://voxcharta.org/?s=" + arxiv;
 
