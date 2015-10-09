33c0f2ef070a44c56e7c1dcecc98622f81ed2252
mdtmc <miles.cranmer@gmail.com>
2015-10-09 01:10:57 -0400

    Fixed extra deletion of abs. number

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,6 +1,9 @@
 var url = window.location.href;
 var arxiv = url.substr(url.lastIndexOf('/') + 1);
-arxiv = arxiv.substr(0,arxiv.lastIndexOf('?'));
+if (arxiv.indexOf('?')!= -1)
+{
+	arxiv = arxiv.substr(0,arxiv.lastIndexOf('?'));
+}
 var voxsearch = "<br><a href=http://voxcharta.org/?s=" + arxiv+ "> Voxcharta </a>";
 var voxsearchh = "http://voxcharta.org/?s=" + arxiv;
 
