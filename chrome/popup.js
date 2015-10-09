36f91faaafcac483cfc44a2efe3058f34630b319
mdtmc <miles.cranmer@gmail.com>
2015-10-09 09:54:32 -0400

    Now there is two links on page - one under bookmarks. v0.3

--- chrome/popup.js
+++ chrome/popup.js
@@ -4,9 +4,10 @@ if (arxiv.indexOf('?')!= -1)
 {
 	arxiv = arxiv.substr(0,arxiv.lastIndexOf('?'));
 }
-var voxsearch = "<br><a href=http://voxcharta.org/?s=" + arxiv+ "> Voxcharta </a>";
+var voxsearch = "<br><a href=http://voxcharta.org/?s=" + arxiv+ "> Vox Charta </a>";
 var voxsearchh = "http://voxcharta.org/?s=" + arxiv;
 
 //document.write($('.product h3').load(voxsearchh).text());
 
 document.getElementsByClassName("title mathjax")[0].innerHTML += voxsearch;
+document.getElementsByClassName("bookmarks")[0].innerHTML += voxsearch;
